b53522636e1ce452a3a3e62e7cc178e9ff503fc7
Jon Deaves <hello@jondeaves.me>
2016-01-30 18:21:09 +0000

    Smoother rotation
    Smooth out the rotation after clicking

--- public/constants.js
+++ public/constants.js
@@ -1,8 +1,10 @@
-// Physics
+// World
 var physicsBaseRestitution = 3;           // Bounce strength
+var worldBoundsOffset = 100;            // An offset to stop imp return to centre until totally off screen
 
 
 // Imps
+var rotationChangePerSecond = 0.0025;
 var impBaseThrust = 50;                         // Acceleration
 var impBaseDamping = 6;                   // Deceleration
 var impScaleLimits = [0.08, 0.12];             // Negative value will mess up collision box
